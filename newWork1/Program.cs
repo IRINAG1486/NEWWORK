1e2dbeb7d030a9187919aae8d08621d5960bb96b
IRINAG <ira848@mail.ru>
2023-01-26 23:08:53 +0600

    seminar work

---
+++ newWork1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    int digit_3 = num % 10;
+    console.WriteLine($"Third digit is {digit_3}");     
+}
+else
+{
+    Console.WriteLine("your number is not 3-digit!");
+}
+
+
+
+
+
