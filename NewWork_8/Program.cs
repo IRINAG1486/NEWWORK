46b1f95a8d53240a4415dff3e67e2c354bb6f21c
IRINAG <ira848@mail.ru>
2023-01-28 16:28:17 +0600

    Приветствие разных пользователей

---
+++ NewWork_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача для приветствия разных пользователей
+
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+if(username.ToLower()=="маша")
+{
+   Console.WriteLine("Ура,это же Маша"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
+
+
