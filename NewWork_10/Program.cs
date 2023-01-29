0337ef107f96537dad8075957fc6842422d8eb78
IRINAG <ira848@mail.ru>
2023-01-29 19:10:34 +0600

    Начала задачу с точками

---
+++ NewWork_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача с точками (находить середину между точками и ставить точку)
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 20;
+int xc = 40;
+int yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+Console.WriteLine("Hello, World!");
